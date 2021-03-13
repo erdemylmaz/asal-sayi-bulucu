5fe794d47b4855671e0846985b9077c0c5fab5c4
Erdem Yilmaz <16erdemylmz@gmail.com>
2021-03-13 15:33:48 +0300

    file

---
+++ script.js
@@ -0,0 +1,12 @@
+const output = document.querySelector('.output');
+const form = document.querySelector('form');
+
+let numbers = [];
+
+
+find = (e) => {
+  e.preventDefault();
+
+}
+
+form.addEventListener('submit', find);
